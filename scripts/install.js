b3305509577811ec51e33f0e088951efde33660b
Jimmy Ruts <jimmy.ruts@thomascookonline.com>
2018-06-15 14:14:24 +0200

    added buildspec

--- scripts/install.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process');
-
-execSync('pip install aws-sam-cli');
-execSync('sam package --template-file template.yaml --s3-bucket grimlocks-testing --output-template-file packaged.yaml')
